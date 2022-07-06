1b2bc45d863a06e81419410364e939c151a689c1
Akash <akash101811@gmail.com>
2022-07-06 22:16:56 +0530

    layout

---
+++ Client/front-end/src/component/Home/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './home.css'
+function home() {
+  return (
+    <div className='home-intro'>
+        <h1>FIND INTEREST RATES IN<br/>ONE PLACE.</h1>
+    </div>
+  )
+}
+
+export default home

8c1a556dbb87d44287bbb86002a80b37ae6eccd5
Akash <akash101811@gmail.com>
2022-07-07 20:56:29 +0530

    day2

---
+++ Client/front-end/src/component/Footer/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div>
+        <nav class="navbar bg-black">
+            <div class="container-fluid">
+                <span class="navbar-brand mb-0 h1">Navbar</span>
+            </div>
+</nav>
+    </div>
+  )
+}
+
+export default Footer
